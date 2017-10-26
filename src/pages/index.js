31aafdb552898d8c3043c629e5b3b8e6f4c32ae9
Matt Woods <mattwoods21@gmail.com>
2017-10-26 14:51:20 -0500

    added typography.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,37 @@ import React from 'react';
 import Link from 'gatsby-link';
 
 export default () => (
-	<div style={{ color: 'tomato' }}>
+	<div style={{ color: '#333', margin: '3rem auto', maxWidth: 600 }}>
 		<h1>Hello Gatsby</h1>
 		<p>Still schwifty</p>
+
+		<div>
+			<h1>Richard Hamming on Luck</h1>
+			<div>
+				<p>
+					From Richard Hamming’s classic and must-read talk, “<a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
+						You and Your Research
+					</a>”.
+				</p>
+				<blockquote>
+					<p>
+						There is indeed an element of luck, and no, there isn’t. The
+						prepared mind sooner or later finds something important and does it.
+						So yes, it is luck.{' '}
+						<em>
+							The particular thing you do is luck, but that you do something is
+							not.
+						</em>
+					</p>
+				</blockquote>
+			</div>
+			<p>Posted April 09, 2011</p>
+		</div>
+
 		<img src="http://lorempixel.com/400/200/" alt="" />
 		<br />
 		<Link to="/page-2/">Go to Page 2</Link>
+		<br />
 		<Link to="/counter/">Counter</Link>
 	</div>
 );
